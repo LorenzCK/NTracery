4d09aaecfc06f8d5daed27a3e379a31080b0975e
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-10-18 23:56:29 +0200

    Add example project for testing

---
+++ src/NTracery.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NTracery.Example {
+    class Program {
+        static void Main(string[] args) {
+            var tracery = new NTracery();
+            tracery.LoadGrammar(@"{""sentence"":[""The #color# #animal# of the #natureNoun# is called #name#""],""color"":[""orange"",""blue"",""white"",""black"",""grey"",""purple"",""indigo"",""turquoise""],""animal"":[""unicorn"",""raven"",""sparrow"",""scorpion"",""coyote"",""eagle"",""owl"",""lizard"",""zebra"",""duck"",""kitten""],""natureNoun"":[""ocean"",""mountain"",""forest"",""cloud"",""river"",""tree"",""sky"",""sea"",""desert""],""name"":[""Arjun"",""Yuuma"",""Darcy"",""Mia"",""Chiaki"",""Izzi"",""Azra"",""Lina""]}");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
